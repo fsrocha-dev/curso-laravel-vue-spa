b9bfe4f60dd3bbaf8647299bda8e2c38eaff2471
fsrocha-dev <contato.frankrocha@gmail.com>
2020-05-16 18:24:56 -0300

    adicionado trait api tokens no model do usuario

--- app/User.php
+++ app/User.php
@@ -5,9 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
     use SoftDeletes;
     use Notifiable;
 
