be66c328a1244b78558effc7dba130b2e68a934a
fsrocha-dev <contato.frankrocha@gmail.com>
2020-05-16 15:04:49 -0300

    criado o seed das categories

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $categories = ['Ideas', 'On Going', 'Completed'];
+
+        foreach ($categories as $category) {
+            Category::create(['name' => $category]);
+        }
+    }
+}
