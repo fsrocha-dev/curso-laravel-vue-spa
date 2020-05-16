2bb8450d8120e3e37e369174d4331e3d976824c7
fsrocha-dev <contato.frankrocha@gmail.com>
2020-05-16 15:54:19 -0300

    criaçao das api routes

--- routes/api.php
+++ routes/api.php
@@ -6,13 +6,12 @@ use Illuminate\Http\Request;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/taks', 'TaskController');
+Route::get('/category/{category}/tasks', 'CategoryController@tasks');
+Route::resource('/category', 'CategoryController');
