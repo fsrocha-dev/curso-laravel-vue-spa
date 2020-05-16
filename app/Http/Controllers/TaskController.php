b4e92ba014f47619541924873182b598d06606e4
fsrocha-dev <contato.frankrocha@gmail.com>
2020-05-16 15:59:09 -0300

    criado end point de cadastrar tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,7 +14,6 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
     }
 
     /**
@@ -24,62 +23,64 @@ class TaskController extends Controller
      */
     public function create()
     {
-        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $task = Task::create([
+            'name' => $request->name,
+            'category_id' => $request->category_id,
+            'user_id' => $request->user_id,
+            'order' => $request->order,
+        ]);
+
+        $data = [
+            'data' => $task,
+            'status' => (bool) $task,
+            'message' => $task ? 'Task Created!' : 'Error Creating Task',
+        ];
+
+        return response()->json($data);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Task  $task
      * @return \Illuminate\Http\Response
      */
     public function show(Task $task)
     {
-        //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Task  $task
      * @return \Illuminate\Http\Response
      */
     public function edit(Task $task)
     {
-        //
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Task  $task
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Task $task)
     {
-        //
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Task  $task
      * @return \Illuminate\Http\Response
      */
     public function destroy(Task $task)
     {
-        //
     }
 }
