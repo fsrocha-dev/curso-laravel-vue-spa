5fa7219dc700a03f596f4917a63b783b7f978c73
fsrocha-dev <contato.frankrocha@gmail.com>
2020-05-17 10:29:20 -0300

    registrado rotas dos componentes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,9 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+import Dashboard from './views/Board'
+import Login from './views/Login'
+import Register from './views/Register'
 import Welcome from '../views/Welcome'
 
 const router = new VueRouter({
@@ -12,6 +15,21 @@ const router = new VueRouter({
             path: '/',
             name: 'home',
             component: Welcome
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login,
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register,
+        },
+        {
+            path: '/board',
+            name: 'board',
+            component: Dashboard,
         },
     ],
 });
