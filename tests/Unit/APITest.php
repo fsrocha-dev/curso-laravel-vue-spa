8f79e5cd9d44adf75679a3a51addd7e838bdcdbd
fsrocha-dev <contato.frankrocha@gmail.com>
2020-05-16 21:46:31 -0300

    criado os testes da category

--- tests/Unit/APITest.php
+++ tests/Unit/APITest.php
@@ -34,4 +34,49 @@ class APITest extends TestCase
             'success' => ['token'],
         ]);
     }
+
+    public function testCategoryFetch()
+    {
+        $user = \App\User::find(1);
+
+        $response = $this->actingAs($user, 'api')
+            ->json('GET', '/api/category')
+            ->assertStatus(200)->assertJsonStructure([
+                '*' => [
+                    'id',
+                    'name',
+                    'created_at',
+                    'updated_at',
+                    'deleted_at',
+                ],
+            ]);
+    }
+
+    public function testCategoryCreation()
+    {
+        $this->withoutMiddleware();
+
+        $response = $this->json('POST', '/api/category', [
+            'name' => 'Any category',
+        ]);
+
+        $response->assertStatus(200)->assertJson([
+            'status' => true,
+            'message' => 'Category Created',
+        ]);
+    }
+
+    public function testCategoryDeletion()
+    {
+        $user = \App\User::find(1);
+
+        $category = \App\Category::create(['name' => 'To be deleted']);
+
+        $response = $this->actingAs($user, 'api')
+            ->json('DELETE', "/api/category/{$category->id}")
+            ->assertStatus(200)->assertJson([
+                'status' => true,
+                'message' => 'Category Deleted',
+            ]);
+    }
 }
